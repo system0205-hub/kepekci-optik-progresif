5d66585fe1227e14265cc25077c84543976697cb
system0205-hub <muzafferkepekci@gmail.com>
2026-04-21 05:15:00 +0300

    feat(novax): Classic progresif + ozel amac + miyopi kontrol + bifokal (chunk 2)
    Classic progresif (4 model):
    - Matrix HD YENI (orta, 12.500-62.000TL)
    - Genius YENI (orta, uzak onc., 10.500-60.000TL)
    - Ventro (baslangic, yakin onc., 8.500-21.000TL)
    - Novum NG (baslangic, dengeli, 7.500-20.000TL)
    
    Classic ozel amac (4 model):
    - Sportive Progresif YENI (sport, 17.500-71.000TL)
    - DriveOn Progresif YENI (drive, PixAR DRIVE std, 17.500-71.000TL)
    - Officient (ofis, 7 degresyon, 7.500-33.500TL)
    - Serenity YENI (antifatigue, 6 ADD seviyesi, 8.000-53.000TL)
    
    Miyopi Kontrol (2 model):
    - Myopi-X (yapay zeka, 9.500-36.500TL)
    - Myopi-X C.I.R.C.L.E. YENI (907 mikro lens, 12.000TL)
    
    Bifokal (2 model):
    - Bifokal Standart D28 (7.500-52.500TL)
    - Bifokal FreeForm RS28/40/60 (orta, 7.500-52.500TL)
    
    TOPLAM: Novax 23 model (1'den 23'e - plan'daki 20+ hedefi karsilandi)
    
    SW cache v21 -> v22

--- progresif-oneri/sw.js
+++ progresif-oneri/sw.js
@@ -1,5 +1,5 @@
 // Kepekci Optik - Service Worker (Offline Destek)
-var CACHE_NAME = "kepekci-optik-v21";
+var CACHE_NAME = "kepekci-optik-v22";
 var OFFLINE_URLS = [
   "./index.html",
   "./css/style.css",
